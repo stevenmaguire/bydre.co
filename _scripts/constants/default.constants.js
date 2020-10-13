6d235011b0d099454b3484ef1639b8e48b0282d7
Steven Maguire <stevenmaguire@gmail.com>
2020-10-13 14:18:23 -0500

    Update default.constants.js

--- _scripts/constants/default.constants.js
+++ _scripts/constants/default.constants.js
@@ -2,7 +2,7 @@
    'use strict';
 
     constants.constant('Default', {
-        apiUrl: 'http://service.bydre.co',
+        apiUrl: 'https://service.bydre.co',
         devApiUrl: 'http://localhost:8000',
         cacheTime: 15 * 60 * 1000
     });
